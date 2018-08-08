aa4ccc24273db867cf357f9f86e80632b540250a
someone <someone@someplace.com>
2018-08-09 00:54:11 +0200

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -30,6 +31,30 @@ app.get("/todos", (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    //check valid
+    //404  send empty
+    //query find by id send 
+    // if error send 400
+    // if success send back if no todo send 404 empty
+
+    // is this bad boi valid?
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((doc) => {
+        if (!doc) {
+            res.status(404).send();
+        }
+        res.status(200).send({ doc });
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
