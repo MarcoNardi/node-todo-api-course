b5638a21b7116a434ec8c8d5b70b64833b377688
someone <someone@someplace.com>
2018-08-09 00:17:41 +0200

    add GET /todos testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require("supertest");
 const { app } = require("./../server.js");
 const { Todo } = require("./../models/todo");
 
+const todos = [
+    {
+        text: "first test todo",
+    }, {
+        text: "second test todo "
+    }
+]
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe("post/Todos", () => {
@@ -17,7 +27,7 @@ describe("post/Todos", () => {
             if (err) {
                 return done(err);
             }
-            Todo.find().then((todos) => {
+            Todo.find({ text }).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -31,9 +41,18 @@ describe("post/Todos", () => {
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
         });
+    });
+
+});
+
+describe("GET /todos route", (done) => {
+    it("should get all todos", () => {
+        request(app).get("/todos").expect(200).expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        }).end(done);
     });
 });
