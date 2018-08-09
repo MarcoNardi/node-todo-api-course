7dbb6bb3340fb61f3356e9bd7ad8b77a596d1593
someone <someone@someplace.com>
2018-08-09 23:56:54 +0200

    preparing for heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
 
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(bodyParser.json());
@@ -51,8 +53,8 @@ app.get("/todos/:id", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("server started on port 3000");
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
 })
 
 module.exports = { app };
