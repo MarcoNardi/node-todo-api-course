b7b1e76cacdde9064001e8c82e48257c18d18f21
someone <someone@someplace.com>
2018-08-13 23:12:53 +0200

    add /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,25 @@ schema.methods.generateAuthToken = function () {
     return user.save().then(() => { return token });
 };
 
+schema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 schema.statics.findByToken = function (token) {
     var User = this;
     var decoded
